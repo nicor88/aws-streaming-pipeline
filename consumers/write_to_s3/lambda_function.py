0b6830b81e0155afae3e5924b9cd3f06a4cc4725
nicor88 <nico@8fit.com>
2018-11-17 14:20:32 +0100

    add reader records

--- consumers/write_to_s3/lambda_function.py
+++ consumers/write_to_s3/lambda_function.py
@@ -1,4 +1,6 @@
+import base64
 import logging
+import json
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -6,11 +8,24 @@ logger.setLevel(logging.INFO)
 logger.info('Loading function')
 
 
-def read_records():
-    return []
+def read_and_validate(record):
+    decoded_record = json.loads(base64.b64decode(record['kinesis']['data']).decode())
+    decoded_record['sequenceNumber'] = record['kinesis']['sequenceNumber']
+    return decoded_record
+
+
+def read_records(records):
+    if not records:
+        return []
+    records_acc = (read_and_validate(record) for record in records)
+    return records_acc
 
 
 def lambda_handler(event, context):
     logger.info(event)
     output = {'output': 'hello world'}
+    records = event.get('Records')
+    valid_records = read_records(records)
+    logger.info(f'Read {list(valid_records)}')
+    # TODO write records to S3
     return output
